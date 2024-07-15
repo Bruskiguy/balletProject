5020ed2233953cc19435dacd3d093a43ec6550bf
BruskiMac <caleb.a.bruski@gmail.com>
2024-07-15 15:27:34 +0300

    made some more changes

--- ballet-site/components/ui/dataTable.tsx
+++ ballet-site/components/ui/dataTable.tsx
@@ -1,4 +1,8 @@
 // import { rowDefs, rows } from "../data";
+"use client";
+
+import classNames from "classnames";
+import React from "react";
 
 type RowDef = { name: string; label: string; type: string };
 type Row = Record<string, string | boolean | number>;
@@ -7,9 +11,18 @@ type DataTableProps = {
   rowDefs: RowDef[];
   rows: Row[];
   previewColumns?: string[];
+  editViewType?: "inline" | "expanded";
 };
 
+// create a state for row and row value thats being edited
+//on click make area a text-area
+
 export const DataTable = (props: DataTableProps) => {
+  const [editingCell, setEditingCell] = React.useState<{
+    rowIndex: number;
+    colName: string;
+  }>();
+
   return (
     <table className=" mx-auto border-collapse border-6 bg-gray-600 transition-colors duration-300 ease-in-out cursor-pointer">
       <thead>
@@ -27,17 +40,28 @@ export const DataTable = (props: DataTableProps) => {
         </tr>
       </thead>
       <tbody>
-        {props.rows.map((item, index) => (
+        {props.rows.map((rowItem, index) => (
           <tr
             key={index}
-            className="border-2 hover:bg-gray-300 transition-all duration-200 ease-in-out"
+            className="border- transition-all duration-200 ease-in-out"
           >
             {(props.previewColumns
               ? props.previewColumns
-              : Object.keys(item)
-            ).map((col, i) => (
-              <td className="p-3" key={i}>
-                {mapRowValue(item, col, props.rowDefs)}
+              : Object.keys(rowItem)
+            ).map((columnName, i) => (
+              <td
+                className="hover:bg-gray-700 rounded-md p-3"
+                key={i}
+                onClick={() =>
+                  setEditingCell({ rowIndex: index, colName: columnName })
+                }
+              >
+                {editingCell?.colName === columnName &&
+                editingCell?.rowIndex === index ? (
+                  <EditCell row={rowItem} columnName={columnName} />
+                ) : (
+                  getCellPreviewData(rowItem, columnName, props.rowDefs)
+                )}
               </td>
             ))}
           </tr>
@@ -47,8 +71,25 @@ export const DataTable = (props: DataTableProps) => {
   );
 };
 
+type EditCellProps = {
+  row: Row;
+  columnName: string;
+};
+
+export const EditCell = ({ row, columnName }: EditCellProps) => {
+  return (
+    <>
+      <input
+        className="bg-gray-600 border border-gray-300 text-gray-100 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        name="cellContent"
+        defaultValue={String(row[columnName])}
+      />
+    </>
+  );
+};
+
 // const mapRowValue = (row: Row, columnName: string, rowDefs: RowDef[]) => {
-function mapRowValue(row: Row, columnName: string, rowDefs: RowDef[]) {
+function getCellPreviewData(row: Row, columnName: string, rowDefs: RowDef[]) {
   const value = row[columnName];
   const thisRowDef = rowDefs.find((def) => def.name === columnName);
 
